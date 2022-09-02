719af44c2bf31462685a8b8514660250ffac91ec
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-09-02 11:52:25 +0900

    주문 조회 V2:엔티티를 DTO로 변환

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -31,6 +31,50 @@ public class OrderApiController {
 
     }
 
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
 
+        return result;
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getItem().getPrice();
+            count = orderItem.getCount();
+        }
+    }
 
 }
